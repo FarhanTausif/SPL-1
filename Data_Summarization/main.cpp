2ab12a5b9eb80c95351ac66ba37a13816faefdc6
FarhanTausif <bsse1440@iit.du.ac.bd>
2023-08-30 01:02:02 +0600

    main

---
+++ Data_Summarization/main.cpp
@@ -0,0 +1,22 @@
+#include "src/one_variable.cpp"
+#include "src/data_summarization.cpp"
+
+using namespace std;
+
+int main(){ 
+    
+    vector<double> x;
+    int datasetSize;
+    
+    cout << "---------------MODULE-1: DATA SUMMARIZATION---------------" << endl;
+    
+    cout << "Enter the size of one variable dataset: " << endl;
+    cin >> datasetSize;
+    x.resize(datasetSize);
+
+    generateOneVariableDataset(x); 
+    writeOneVariableDatasetIntoFile(x);
+    summarizeDataset(x,datasetSize);
+
+    return 0;
+}
