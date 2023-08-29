bf154742800cdb371511780a6da3ac8b91ca7e1b
FarhanTausif <bsse1440@iit.du.ac.bd>
2023-08-30 00:47:26 +0600

    "Deleted previous structure of the module"

--- Data_Summerization/dataSummarization.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-double calculateMean(vector<double> &x);
-double calculateMedian(vector<double> &x);
-vector<double> calculateMode(vector<double> &x);
-void summarizeDataset(vector<double> &x,int size);
