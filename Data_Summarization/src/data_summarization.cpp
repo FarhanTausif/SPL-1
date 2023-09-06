ff6c14c413f7d5ce2af4524ca4e02e130d96f057
FarhanTausif <bsse1440@iit.du.ac.bd>
2023-09-07 02:39:46 +0600

    Updated source code for data summarization

--- Data_Summarization/src/data_summarization.cpp
+++ Data_Summarization/src/data_summarization.cpp
@@ -134,21 +134,31 @@ double calculateStandardDeviation(vector<double> &x){
 }
 
 
+double calculateCoefficientOfVariation(vector<double> &x){
+    double standardDeviation = calculateStandardDeviation(x);
+    double mean = calculateMean(x);
+    double coefficientOfVariation = ( standardDeviation / mean ) * 100.0;
+    return coefficientOfVariation;
+}
+
 double calculatePercentile(vector<double> &x, double percentile){
-    //sort
-    sort(x.begin(), x.end());
-    int sampleSize = x.size();
-    double percentileValue;
-    double rank = (sampleSize + 1) * (percentile / 100.0);
+    double percentileValue = 0.0;
+    if(percentile > 0.0 && percentile <=100.0){
+        //sort
+        sort(x.begin(), x.end());
+        int sampleSize = x.size();
+        double rank = (sampleSize + 1) * (percentile / 100.0);
 
-    //check whether rank is a whole number or not 
-    if(rank - floor(rank) == 0.0){
-        percentileValue = x[rank-1];
-    }
-    else{
-        percentileValue = (x[rank-1] + x[rank]) / 2.0;
-    }
+        //check whether rank is a whole number or not 
+        if(rank - floor(rank) == 0.0){
+            percentileValue = x[rank-1];
+        }
+        else{
+            percentileValue = (x[rank-1] + x[rank]) / 2.0;
+        }
 
+        return percentileValue;        
+    }
     return percentileValue;
 }
 
@@ -157,49 +167,132 @@ vector<double> calculateQuartiles(vector<double> &x){
     vector<double> quartiles;
     quartiles.resize(3);
 
-    quartiles[0] = calculatePercentile(x, 25.0);
-    quartiles[1] = calculatePercentile(x, 50.0);
-    quartiles[2] = calculatePercentile(x, 75.0);
+    double percentile;
+    for(int i = 0, percentile = 25.0; i < quartiles.size(); ++i, percentile += 25.0){
+        quartiles[i] = calculatePercentile(x, percentile);
+    }
 
     return quartiles;
 }
 
-vector<double> detectOutliers(vector<double>& x){
+vector<double> calculateDeciles(vector<double> &x){
+
+    vector<double> deciles;
+    deciles.resize(9);
+    double percentile;
+
+    for(int i = 0, percentile = 10.0; i < deciles.size(); ++i, percentile += 10.0){
+        deciles[i] = calculatePercentile(x, percentile);
+    }
+
+    return deciles; 
+}
+
+vector<double> detectOutliersUsingInterQuartileRange(vector<double>& x){
     
-    vector<double> outliers;
+    vector<double> outliersUsingInterQuartileRange;
 
     vector<double> quartiles = calculateQuartiles(x);
     double q1 = quartiles[0];
     double q3 = quartiles[2];
+
+    double interQuartileRange = q3 - q1;
+    double lowerBound = q1 - 1.5 * interQuartileRange;
+    double upperBound = q3 + 1.5 * interQuartileRange;
     
-    // Calculate Inter-Quartile Range(IQR)
-    double IQR = q3 - q1;
-    
-    // Define lower and upper bounds for outliers
-    double lowerBound = q1 - 1.5 * IQR;
-    double upperBound = q3 + 1.5 * IQR;
-    
-    // Detect outliers
     for (double value : x) {
         if (value < lowerBound || value > upperBound) {
-            outliers.push_back(value);
+            outliersUsingInterQuartileRange.push_back(value);
         }
     }
     
-    return outliers;
+    return outliersUsingInterQuartileRange;
+}
+
+double calculateZScore(double value, double mean, double standardDeviation){
+    double zScore = (value - mean) / standardDeviation;
+    return zScore;
+}
+
+vector<double> detectOutliersUsingZScore(vector<double> &x){
+    
+    vector<double> outliersUsingZScore; 
+    double mean;
+    double standardDeviation;
+    double zScore;
+    double thresholdStdDevUnit = 3.0;
+    
+    mean = calculateMean(x);
+    standardDeviation = calculateStandardDeviation(x);
+
+    for(double value : x){
+        zScore = calculateZScore(value, mean, standardDeviation);  
+        if(zScore > thresholdStdDevUnit){
+            outliersUsingZScore.push_back(value);
+        }
+    }
+
+    return outliersUsingZScore;
 }
 
 
-void summarizeDataset(vector<double> &x,int size){
-    
+double calculateSkewnessUsingPearsonsFirstMeasure(vector<double> &x, double mode){
+    double skewness;
+    double mean = calculateMean(x);
+    double standardDeviation = calculateStandardDeviation(x);
+    skewness = (mean - mode)/standardDeviation; 
+    return skewness;
+}
+
+double calculateSkewnessUsingPearsonsSecondMeasure(vector<double> &x){
+    double skewness;
+    double mean = calculateMean(x);
+    double median = calculateMedian(x);
+    double standardDeviation = calculateStandardDeviation(x);
+    skewness = (3 * (mean - median))/standardDeviation; 
+    return skewness;
+}
+
+double calculateQuartileDeviation(vector<double> &x){
+    double quartileDeviation;
+    vector<double> quartiles = calculateQuartiles(x);
+    double q1 = quartiles[0];
+    double q3 = quartiles[2];
+    quartileDeviation = (q3 - q1)/2.0;
+    return quartileDeviation;
+}
+
+double calculateKurtosis(vector<double> &x){
+    double kurtosis;
+    double quartileDeviation = calculateQuartileDeviation(x);
+    double p90 = calculatePercentile(x, 90.0);
+    double p10 = calculatePercentile(x, 10.0);
+    kurtosis = quartileDeviation / (p90 - p10);
+    return kurtosis;
+}
+
+
+void summarizeOneVariableDataset(vector<double> &x,int size){
+
     double mean = calculateMean(x);
     double median = calculateMedian(x);
     vector<double>modes = calculateMode(x);
     double range = calculateRange(x);
     double variance = calculateVariance(x);
     double standardDeviation = calculateStandardDeviation(x);
+    double coefficientOfVariation = calculateCoefficientOfVariation(x);
     vector<double> quartiles = calculateQuartiles(x);
-    vector<double> outliers = detectOutliers(x);
+    vector<double> deciles = calculateDeciles(x);
+    vector<double> outliersUsingInterQuartileRange = detectOutliersUsingInterQuartileRange(x);
+    vector<double> outliersUsingZScore = detectOutliersUsingZScore(x);
+    double skewness;
+    if(modes.size() == 1){
+        skewness = calculateSkewnessUsingPearsonsFirstMeasure(x, modes[0]);
+    }
+    else{
+        skewness = calculateSkewnessUsingPearsonsSecondMeasure(x);
+    }
+    double kurtosis = calculateKurtosis(x);
 
     setprecision(4);
     cout << "\n::::Summary based on Measures of Central Tendency::::" << endl;
@@ -207,11 +300,23 @@ void summarizeDataset(vector<double> &x,int size){
     cout << "Sample Median: " << median << endl;
 
     if(!modes.empty()){
+        
         cout << "Sample Mode(s): ";
         for(double mode : modes){
             cout << mode << " ";
         }
         cout << endl;
+
+        if(modes.size() == 1){
+            cout << "The Dataset is Uni-modal" << endl;
+        }
+        else if(modes.size() == 2){
+            cout << "The Dataset is Bi-modal" << endl;
+        }
+        else{
+            cout << "The Dataset is Tri-modal" << endl;
+        }
+
     }
     else{
         cout << "No modes!" << endl;
@@ -219,20 +324,68 @@ void summarizeDataset(vector<double> &x,int size){
 
     cout << endl << endl;
     
-    cout << "::::Summary based on Measures of Disperse::::" << endl;
+    cout << "::::Summary based on Measures of Dispersion::::" << endl;
     cout << "Range of the dataset: " << range << endl; 
     cout << "Sample Variance: " << variance << endl;
     cout << "Sample Std. Deviation: " << standardDeviation << endl;
+    cout << "Minimum data point: " << getMinimum(x) << endl; 
     cout << "Quartiles:" << endl;
-    cout << "Q1 = " << quartiles[0] << " Q2 = " << quartiles[1] << " Q3 = " << quartiles[2] << endl;
+    cout << "Lower Quartile, Q1 = " << quartiles[0] << "  Middle Quartile, Q2 = " << quartiles[1] << "  Upper Quartile, Q3 = " << quartiles[2] << endl;
+    cout << "Maximum data point:" << getMaximum(x) << endl;
+    cout << "Deciles:" << endl;
+    for(double decile: deciles){
+        cout << decile << " ";
+    }
+    
+    cout << endl << endl;
 
-    cout << "Outliers:" << endl;
+    cout << "::::Measures of Asymmetry & Tails heaviness or lightness compared to normal distribution::::" << endl;
+    if(skewness > -0.5 && skewness < 0.5){
+        cout << "The dataset is nearly symmetrical" << endl;
+    }
+    else if(skewness > -1.0 && skewness < -0.5){
+        cout << "The dataset is slightly negatively skewed!" << endl;
+    }
+    else if(skewness > 0.5 && skewness < 1.0){
+        cout << "The dataset is slightly positively skewed!" << endl;
+    }
+    else if(skewness < -1.0){
+        cout << "The dataset is extremely negatively skewed!" << endl;
+    }
+    else if(skewness > 1.0){
+        cout << "The dataset is extremely positively skewed!" << endl;
+    }
 
-    if(outliers.empty()){
-        cout << "No outliers detected!" << endl;
+    if(kurtosis > 2.8 && kurtosis < 3.2){
+        cout << "The distribution of the dataset is: Mesokurtic(Normal Distribution)" << endl;
+    }
+    else if(kurtosis > 3.2){
+        cout << "The distribution of the dataset is: Leptokurtic(Long tailed distribution :: higher outliers)" << endl;
+    }
+    else if(kurtosis < 2.8){
+        cout << "The distribution of the dataset is: Platykurtic(short tailed distribution :: fewer outliers)" << endl;
+    }
+
+    cout << endl;
+
+    cout << "::::Outlier Detection::::" << endl;
+    cout << "Outliers(" << outliersUsingInterQuartileRange.size() << " values)" << " Using inter quartile range:"   << endl;
+    if(outliersUsingInterQuartileRange.empty()){
+        cout << "No outliers detected using IQR method!" << endl;
     }
     else{
-        for(double outlier : outliers){
+        for(double outlier : outliersUsingInterQuartileRange){
+            cout << outlier << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << "Outliers(" << outliersUsingZScore.size() << " values)" << " Using z-score:"   << endl;
+    if(outliersUsingZScore.empty()){
+        cout << "No outliers detected using z-score method!" << endl;
+    }
+    else{
+        for(double outlier : outliersUsingZScore){
             cout << outlier << " ";
         }
         cout << endl;
