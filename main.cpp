92495fd230c68718712452a94a6f330e4344a8d1
FarhanTausif <bsse1440@iit.du.ac.bd>
2023-09-07 16:11:12 +0600

    main

--- main.cpp
+++ main.cpp
@@ -1,22 +1,49 @@
 #include "Data_Summarization/src/one_variable.cpp"
+#include "Data_Summarization/src/two_variable.cpp"
 #include "Data_Summarization/src/data_summarization.cpp"
 
 using namespace std;
 
 int main(){ 
-    
+
     vector<double> x;
-    int datasetSize;
-    
-    cout << "---------------   DATA SUMMARIZATION   ---------------" << endl;
-    
-    cout << "Enter the size of one variable dataset: " << endl;
-    cin >> datasetSize;
-    x.resize(datasetSize);
+    vector<double> y;
+    int twoVariableDatasetSize;
+    int choice; 
 
-    generateOneVariableDataset(x); 
-    writeOneVariableDatasetIntoFile(x);
-    summarizeOneVariableDataset(x,datasetSize);
+    cout << "------------------------------   DATA SUMMARIZATION   ------------------------------" << endl;
+    
+    cout << "Enter your choice:::Press 1 for One Variable Data summarization\nOr,\nPress 2 for Two Variable Data summarization" << endl;
+    cin >> choice;
+    switch (choice){
+    case 1:
 
+        int oneVariableDatasetSize; 
+        cout << "Enter the size of one variable dataset: " << endl;
+        cin >> oneVariableDatasetSize;
+        x.resize(oneVariableDatasetSize);
+
+        generateOneVariableDataset(x); 
+        writeOneVariableDatasetIntoFile(x);
+        summarizeOneVariableDataset(x,oneVariableDatasetSize);
+    
+        break;
+
+    case 2:
+
+        int twoVariableDatasetSize; 
+        cout << "Enter the size of two variable dataset: " << endl;
+        cin >> twoVariableDatasetSize;
+        x.resize(twoVariableDatasetSize);
+        y.resize(twoVariableDatasetSize);
+
+        generateTwoVariableDataset(x, y); 
+        writeTwoVariableDatasetIntoFile(x, y);
+        break;
+
+    default:
+        break;
+    }
+    
     return 0;
 }
