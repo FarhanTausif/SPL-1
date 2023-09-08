cc0a92ce6d90a10f8a06b62fc9a33547f9a581d5
FarhanTausif <bsse1440@iit.du.ac.bd>
2023-09-09 03:53:11 +0600

    main

--- main.cpp
+++ main.cpp
@@ -8,41 +8,44 @@ int main(){
 
     vector<double> x;
     vector<double> y;
-    int twoVariableDatasetSize;
     int choice; 
+    bool continueLoop = true;
 
-    cout << "------------------------------   DATA SUMMARIZATION   ------------------------------" << endl;
-    
-    cout << "Enter your choice:::Press 1 for One Variable Data summarization\nOr,\nPress 2 for Two Variable Data summarization" << endl;
-    cin >> choice;
-    switch (choice){
-    case 1:
+    cout << "------------------------------------------------------------   DATA SUMMARIZATION   ------------------------------------------------------------" << endl;
+    cout << "1.Single-Variable Data summarization\n2.Two-Variable Data/Paired Data summarization\n3.Quit(Press any numbers other than 1 & 2)" << endl;
+        
+    while(continueLoop){
+        cout << "Enter your choice:" << endl;
+        cin >> choice;
+        switch (choice){
+            case 1:
+                int oneVariableDatasetSize; 
+                cout << "Enter the size of one variable dataset: " << endl;
+                cin >> oneVariableDatasetSize;
+                x.resize(oneVariableDatasetSize);
 
-        int oneVariableDatasetSize; 
-        cout << "Enter the size of one variable dataset: " << endl;
-        cin >> oneVariableDatasetSize;
-        x.resize(oneVariableDatasetSize);
+                generateOneVariableDataset(x); 
+                writeOneVariableDatasetIntoFile(x);
+                summarizeOneVariableDataset(x,oneVariableDatasetSize);
+            
+                break;
 
-        generateOneVariableDataset(x); 
-        writeOneVariableDatasetIntoFile(x);
-        summarizeOneVariableDataset(x,oneVariableDatasetSize);
-    
-        break;
+            case 2:
+                int twoVariableDatasetSize; 
+                cout << "Enter the size of two variable dataset: " << endl;
+                cin >> twoVariableDatasetSize;
+                x.resize(twoVariableDatasetSize);
+                y.resize(twoVariableDatasetSize);
 
-    case 2:
+                generateTwoVariableDataset(x, y, twoVariableDatasetSize); 
+                writeTwoVariableDatasetIntoFile(x, y);
+                summarizeTwoVariableDataset(x,y,twoVariableDatasetSize);
+                break;
 
-        int twoVariableDatasetSize; 
-        cout << "Enter the size of two variable dataset: " << endl;
-        cin >> twoVariableDatasetSize;
-        x.resize(twoVariableDatasetSize);
-        y.resize(twoVariableDatasetSize);
-
-        generateTwoVariableDataset(x, y); 
-        writeTwoVariableDatasetIntoFile(x, y);
-        break;
-
-    default:
-        break;
+            default:
+                continueLoop = false;
+                break;
+        }
     }
     
     return 0;
