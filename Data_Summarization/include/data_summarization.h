68ef2625f866f051fe2fa851f02a87eddf475cc2
FarhanTausif <bsse1440@iit.du.ac.bd>
2023-09-09 03:54:29 +0600

    Updated header file for data summarization

--- Data_Summarization/include/data_summarization.h
+++ Data_Summarization/include/data_summarization.h
@@ -1,89 +1,107 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include <vector>
+#include<climits>
+#include<iomanip> // to use setPrecision(value)
 
-/// @brief summarize dataset based on the measures of Central Tendency
-/// @param x vector of the dataset
+using std::vector;
+using std:: cout;
+using std:: setprecision;
+using std:: endl;
+
+/// @brief calculates sample mean
+/// @param x vector of the single-variable dataset
 /// @return mean
 double calculateMean(vector<double> &x);
 
-/// @brief summarize dataset based on the measures of Central Tendency
-/// @param x vector of the dataset
+/// @brief calculates sample median
+/// @param x vector of the single-variable dataset
 /// @return median
 double calculateMedian(vector<double> &x);
 
-/// @brief summarize dataset based on the measures of Central Tendency
-/// @param x vector of the dataset
+/// @brief calculates sample mode
+/// @param x vector of the single-variable dataset
 /// @return mode
 vector<double> calculateMode(vector<double> &x);
 
 /// @brief find the minimum value
-/// @param x vector of the dataset
+/// @param x vector of the single-variable dataset
 /// @return minimum value
 double getMinimum(vector<double> &x);
 
 /// @brief find the maximum value
-/// @param x vector of the dataset
+/// @param x vector of the single-variable dataset
 /// @return maximum value
 double getMaximum(vector<double> &x);
 
-/// @brief summarize dataset based on the measures of Dispersion
-/// @param x vector of the dataset
+/// @brief calculates range
+/// @param x vector of the single-variable dataset
 /// @return measures of Dispersion(range, variance, standard deviation)
 double calculateRange(vector<double> &x);
 
-/// @brief summarize dataset based on the measures of Dispersion
-/// @param x vector of the dataset
+/// @brief calculates sample variance
+/// @param x vector of the single-variable dataset
 /// @return variance
 double calculateVariance(vector<double> &x);
 
-/// @brief summarize dataset based on the measures of Dispersion
-/// @param x vector of the dataset
+/// @brief calculates sample standard deviation
+/// @param x vector of the single-variable dataset
 /// @return standard deviation
 double calculateStandardDeviation(vector<double> &x);
 
 /// @brief calculates the CV
-/// @param x vector of the dataset
+/// @param x vector of the single-variable dataset
 /// @return coefficient of variation 
 double calculateCoefficientOfVariation(vector<double> &x);
 
-/// @brief calculate percentile 
-/// @param x vector of the dataset
+/// @brief calculates percentile 
+/// @param x vector of the single-variable dataset
 /// @param percentile 
 /// @return percentileValue
 double calculatePercentile(vector<double> &x, double percentile);
 
-/// @brief calculate quartile 
-/// @param x vector of the dataset 
+/// @brief calculates quartile 
+/// @param x vector of the single-variable dataset 
 /// @return quartiles
 vector<double> calculateQuartiles(vector<double> &x);
 
-/// @brief calculate deciles
-/// @param x vector of the dataset
+/// @brief calculates deciles
+/// @param x vector of the single-variable dataset
 /// @return deciles
 vector<double> calculateDeciles(vector<double> &x);
 
 /// @brief calculates skewness of uni-modal data 
-/// @param x vector of the dataset
+/// @param x vector of the single-variable dataset
 /// @return skewness
 double calculateSkewnessUsingPearsonsFirstMeasure(vector<double> &x, double mode);
 
 /// @brief calculates skewness of multi-modal data 
-/// @param x vector of the dataset
+/// @param x vector of the single-variable dataset
 /// @return skewness
 double calculateSkewnessUsingPearsonsSecondMeasure(vector<double> &x);
 
+/// @brief Applies Empirical Rules on Normal Dataset
+/// @param x vector of the single-variable dataset
+void applyEmpiricalRule(vector<double> &x);
+
 /// @brief calculates Quartile Deviation
-/// @param x vector of the dataset 
+/// @param x vector of the single-variable dataset 
 /// @return Quartile Deviation
 double calculateQuartileDeviation(vector<double> &x);
 
+/// @brief calculates Coefficient Of Quartile Deviation 
+/// @param x vector of the single-variable dataset
+/// @return Coefficient of Quartile Deviation
+double calculateCoefficientOfQuartileDeviation(vector<double> &x);
+
 /// @brief calculates kurtosis 
-/// @param x vector of the dataset
+/// @param x vector of the single-variable dataset
 /// @return kurtosis
 double calculateKurtosis(vector<double> &x);
 
 /// @brief detect outliers based on inter quartile range, if the dataset has any
-/// @param x vector of the dataset
+/// @param x vector of the single-variable dataset
 /// @return outliers 
 vector<double> detectOutliersUsingInterQuartileRange(vector<double> &x);
 
@@ -93,17 +111,36 @@ vector<double> detectOutliersUsingInterQuartileRange(vector<double> &x);
 double calculateZScore(double value, double mean, double standardDeviation);
 
 /// @brief detect outliers based on z-statistic, if the dataset has any
-/// @param x vector of the dataset
+/// @param x vector of the single-variable dataset
 /// @return outliers  
 vector<double> detectOutliersUsingZScore(vector<double> &x);
 
+
+/// @brief calculates sample covariance
+/// @param x vector of X variable
+/// @param y vector of Y variable 
+/// @return covariance
+double calculateCovariance(vector<double> &x, vector<double> &y);
+
+/// @brief calculates Pearson's corelation coefficient 
+/// @param x vector of X variable
+/// @param y vector of Y variable
+/// @return pearson's corelation coefficient
+double calculatePerasonsCorelationCoefficient(vector<double> &x, vector<double> &y);  
+
+/// @brief calculates Cohen's d as an effect size measure
+/// @param x vector of X variable
+/// @param y vector of Y variable 
+/// @return cohen's d
+double calculateCohensD(vector <double> &x, vector<double> &y);
+
 /// @brief Summarizes One-Variable dataset
-/// @param x vector of the dataset
+/// @param x vector of the single-variable dataset
 /// @param size 
 void summarizeOneVariableDataset(vector<double> &x,int size);
 
 /// @brief summarizes Two-Variable dataset
-/// @param x vector of the dataset of X-Variable
-/// @param y vector of the dataset of Y-Variable
-/// @param size 
+/// @param x vector of the single-variable dataset of X-Variable
+/// @param y vector of the single-variable dataset of Y-Variable
+/// @param size size of each vector
 void summarizeTwoVariableDataset(vector<double> &x, vector<double> &y, int size);
